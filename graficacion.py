d44fe629529741ff14bf2fe73cc9467639734aef
Luis V <luis_villa_salas@hotmail.com>
2023-06-29 18:55:23 -0600

    batch file to start Mongo and API

--- graficacion.py
+++ graficacion.py
@@ -1,3 +1,4 @@
+import subprocess
 import pymongo
 import matplotlib.pyplot as plt
 
@@ -12,7 +13,7 @@ fig, ax = plt.subplots()
 # Función para actualizar el gráfico en tiempo real
 def update_plot():
     # Obtener los datos más recientes de la base de datos
-    latest_data = collection.find().sort('_id', pymongo.DESCENDING).limit(100)
+    latest_data = collection.find().limit(100)
     x = []
     # y = []
     for data in latest_data:
@@ -30,3 +31,9 @@ while True:
     update_plot()
     plt.pause(1)  # Pausa de 1 segundo entre actualizaciones
 
+    # Comprobar si se ha cerrado la ventana de la gráfica
+    if not plt.get_fignums():
+        break
+
+# Cerrar la conexión a la base de datos
+client.close()
---
+++ start_app.bat
@@ -0,0 +1,19 @@
+@echo off
+
+C:
+cd \Users\luis_
+start /min cmd /k "mongod"
+
+D:
+cd \Tesis\api-arduino
+start /min cmd /c "npm run start"
+
+REM Mensaje personalizado antes de la espera
+echo Se esta ejecutando la base de datos y el servicio de API para enviar los datos a MONGO.
+echo Para cerrar las terminales se presiona cualquier tecla :).
+
+REM Espera a que el usuario presione una tecla para detener el proceso
+pause
+
+taskkill /f /im mongod.exe
+taskkill /t /f /im cmd.exe
